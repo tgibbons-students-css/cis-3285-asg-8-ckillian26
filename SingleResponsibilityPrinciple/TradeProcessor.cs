817cf66f44cf74d46d0e7123fe6066bd015f6067
Chris Killian <ckillian@css.edu>
2018-10-29 20:36:03 -0500

    Added Unit Tests

--- SingleResponsibilityPrinciple/TradeProcessor.cs
+++ SingleResponsibilityPrinciple/TradeProcessor.cs
@@ -114,10 +114,11 @@ namespace SingleResponsibilityPrinciple
             //    The @ sign allows for back slashes
             //    Watch for double quotes which must be escaped using "" 
             //    Watch for extra spaces after C: and avoid paths with - hyphens -
-            using (var connection = new System.Data.SqlClient.SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\ckillian\source\repos\cis-3285-asg-8-ckillian26\tradedatabase.mdf"";Integrated Security=True;Connect Timeout=30;"))
-            //    using (var connection = new System.Data.SqlClient.SqlConnection(@"  ;"))
-            //   using (var connection = new System.Data.SqlClient.SqlConnection("Data Source=(local);Initial Catalog=TradeDatabase;Integrated Security=True;"))
+           
+            using (var connection = new System.Data.SqlClient.SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\chris\source\repos\tgibbons-students-css\cis-3285-asg-8-ckillian26\tradedatabase.mdf;Integrated Security=True;Connect Timeout=30;"))
+       
             {
+                   
                 connection.Open();
                 using (var transaction = connection.BeginTransaction())
                 {
--- SingleResponsibilityPrincipleTests/TradeProcessorTests.cs
+++ SingleResponsibilityPrincipleTests/TradeProcessorTests.cs
@@ -3,6 +3,7 @@ using SingleResponsibilityPrinciple;
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -13,31 +14,88 @@ namespace SingleResponsibilityPrinciple.Tests
     [TestClass()]
     public class TradeProcessorTests
     {
-        [TestMethod()]
-        public void TestNormalFile()
-        {
-            //Arrange
-            var tradeStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("SingleResponsibilityPrinciple.trades.txt");
-            var tradeProcessor = new TradeProcessor();
-           
-            //Assert
-            tradeProcessor.ProcessTrades(tradeStream);
-            //Act
-            int count = CountDbRecords();
-            Assert.AreEqual(count, 4);
-        }
+        
+
         private int CountDbRecords()
         {
-            using (var connection = new System.Data.SqlClient.SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\ckillian\source\repos\cis-3285-asg-8-ckillian26\tradedatabase.mdf"";Integrated Security=True;Connect Timeout=30;"))
+            using (var connection = new System.Data.SqlClient.SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\chris\source\repos\tgibbons-students-css\cis-3285-asg-8-ckillian26\tradedatabase.mdf;Integrated Security=True;Connect Timeout=30;"))
             {
                 connection.Open();
                 string myScalarQuery = "SELECT COUNT(*) FROM trade";
                 SqlCommand myCommand = new SqlCommand(myScalarQuery, connection);
-                myCommand.Connection.Open();
                 int count = (int)myCommand.ExecuteScalar();
                 connection.Close();
                 return count;
             }
         }
+        [TestMethod]
+        public void TestNormalFile()
+        {
+            //Arrange
+            var tradeStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("SingleResponsibilityPrincipleTests.trades.txt");
+            var tradeProcessor = new TradeProcessor();
+            int startCount = CountDbRecords();
+            //Act
+            tradeProcessor.ProcessTrades(tradeStream);
+            int endCount = CountDbRecords();
+
+            //Assert
+            int count = CountDbRecords();
+            Assert.AreEqual(endCount - startCount, 4);
+        }
+        [TestMethod]
+        public void OnlyOneRecordAdded()
+        {
+            //Arrange
+            var tradeStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("SingleResponsibilityPrincipleTests.trades.txt");
+            var tradeProcessor = new TradeProcessor();
+            int startCount = CountDbRecords();
+            //Act
+            tradeProcessor.ProcessTrades(tradeStream);
+            int endCount = CountDbRecords();
+
+            //Assert
+            int count = CountDbRecords();
+            Assert.AreEqual(endCount - startCount, 1);
+        }
+        [TestMethod]
+        public void NoRecordAdded()
+        {
+            //Arrange
+            var tradeStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("SingleResponsibilityPrincipleTests.trades.txt");
+            var tradeProcessor = new TradeProcessor();
+            int startCount = CountDbRecords();
+            //Act
+            tradeProcessor.ProcessTrades(tradeStream);
+            int endCount = CountDbRecords();
+
+            //Assert
+            int count = CountDbRecords();
+            Assert.AreEqual(endCount - startCount, null);
+        }
+        [TestMethod]
+        public void CorrectFile()
+        {
+            //Arrange
+        
+            var tradeStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("SingleResponsibilityPrincipleTests.trades.txt");
+            var tradeProcessor = new TradeProcessor();
+
+            //Assert
+           
+            Assert.AreEqual(tradeStream, tradeStream);
+        }
+        [TestMethod]
+        public void FileExists()
+        {
+            //Arrange
+
+            var tradeStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("SingleResponsibilityPrincipleTests.null.txt");
+            var tradeProcessor = new TradeProcessor();
+
+            //Assert
+            Assert.IsNotNull(tradeStream);
+        }
+
     }
 }
